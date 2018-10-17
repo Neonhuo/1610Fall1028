ff27414d526cfd434177d44eb0ff6ee883fdbaa4
Unknown <williams.perrya@yahoo.com>
2018-10-17 15:35:16 -0600

    We Did More Stuff In Class

---
+++ Physics and Particles/Assets/ChangeValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+//Made by Perry Williams
+public class ChangeValue : ScriptableObject
+{
+    public FloatData Data;
+
+    public void AddValue(FloatData obj)
+    {
+        Data.Value += obj.Value;
+    }
+    
+    public void SubtractValue(FloatData obj)
+    {
+        Data.Value -= obj.Value;
+    }
+    
+}
